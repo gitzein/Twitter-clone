94de809a89e51eccdd3ad8c384db4c04bef7e801
gitzein <zeinfm123@gmai.com>
2024-09-21 18:22:41 +0700

    3- delete post functionality added

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -10,6 +10,6 @@ router.get("/likes/:id", protectRoute, postController.getLikedPosts);
 router.post("/create", protectRoute, postController.createPost);
 router.post("/like/:id", protectRoute, postController.likeOrUnlikePost);
 router.post("/comment/:id", protectRoute, postController.commentPost);
-router.post("/delete/:id", protectRoute, postController.deletePost);
+router.delete("/delete/:id", protectRoute, postController.deletePost);
 
 module.exports = router;
--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -5,19 +5,52 @@ import { FaRegBookmark } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import LoadingSpinner from "./LoadingSpinner";
 
 const Post = ({ post }) => {
   const [comment, setComment] = useState("");
+  const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+  const queryClient = useQueryClient();
   const postOwner = post.user;
   const isLiked = false;
 
-  const isMyPost = true;
+  const isMyPost = post.user._id === authUser._id;
 
   const formattedDate = "1h";
 
   const isCommenting = false;
 
-  const handleDeletePost = () => {};
+  const {
+    mutate: deleteMutation,
+    isPending,
+    data: resMsg,
+    error,
+  } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch(`/api/posts/delete/${post._id}`, {
+          method: "DELETE",
+        });
+        const data = await res.json();
+        if (!res.ok) throw new Error(data.message || "Something went wrong");
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: () => {
+      toast.success("Post deleted");
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+    },
+    onError: () => toast.error(error.message),
+  });
+
+  const handleDeletePost = () => {
+    deleteMutation();
+    console.log(resMsg);
+  };
 
   const handlePostComment = (e) => {
     e.preventDefault();
@@ -50,10 +83,14 @@ const Post = ({ post }) => {
             </span>
             {isMyPost && (
               <span className="flex justify-end flex-1">
-                <FaTrash
-                  className="cursor-pointer hover:text-red-500"
-                  onClick={handleDeletePost}
-                />
+                {isPending ? (
+                  <LoadingSpinner size="sm" />
+                ) : (
+                  <FaTrash
+                    className="cursor-pointer hover:text-red-500"
+                    onClick={handleDeletePost}
+                  />
+                )}
               </span>
             )}
           </div>
